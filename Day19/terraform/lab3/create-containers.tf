bdb8180b678d9dc7eee51e5c157d65f42f09b92a
Jeganathan Swaminathan <mail2jegan@gmail.com>
2020-06-26 06:02:57 +0530

    Added Day20 terraform aws ec2 creation lab exercise.

--- Day19/terraform/lab3/create-containers.tf
+++ Day19/terraform/lab3/create-containers.tf
@@ -2,18 +2,19 @@ provider "docker" {
   host="tcp://localhost:4243" 
 }
 
-resource "docker_image" "tektutor_ansible_ubuntu_image" {
-	name = "tektutor/ansible-ubuntu"
-}
+#resource "docker_image" "tektutor_ansible_ubuntu_image" {
+#	name = "tektutor/ansible-ubuntu"
+#}
 
-resource "docker_image" "tektutor_ansible_centos_image" {
-	name = "tektutor/ansible-centos"
-}
+#resource "docker_image" "tektutor_ansible_centos_image" {
+#	name = "tektutor/ansible-centos"
+#}
 
 resource "docker_container" "ubuntu1_container" {
 	name = "ubuntu-001"
         hostname = "ubuntu-001"
-        image = docker_image.tektutor_ansible_ubuntu_image.name
+        #image = docker_image.tektutor_ansible_ubuntu_image.name
+        image = "tektutor/ansible-ubuntu"
         ports {
 		internal  = "22"
 		external  = "2001"
@@ -27,7 +28,8 @@ resource "docker_container" "ubuntu1_container" {
 resource "docker_container" "centos1_container" {
 	name = "centos1"
         hostname = "centos1"
-        image = docker_image.tektutor_ansible_centos_image.name
+        #image = docker_image.tektutor_ansible_centos_image.name
+        image = "tektutor/ansible-centos"
         ports {
 		internal  = "22"
 		external  = "2002"
